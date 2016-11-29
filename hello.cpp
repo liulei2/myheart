69e2199797f20e2de9e74c5e34f3fc9e4e2daa35
liulei2 <liulei2@pset.suntec.net>
2016-11-29 21:07:48 +0800

    I miss you !

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+/*to my first love*/
+/*I want to know if i can meet you in proper time. */
+#ifndef HELLO_H
+#define HELLO_H
+#endif
+
+#include<stdio.h>
+
+int main()
+{
+	for(int i = 0; i < 80; ++i) {
+		printf("*");
+	}
+
+	printf("\nI love Xiaowen Tang deeply!\n");
+
+	for(int i = 0; i < 80; ++i) {
+		printf("*");
+	}
+	
+	printf("\n");
+	return 0;
+}
